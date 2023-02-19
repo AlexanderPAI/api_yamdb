b3c1a519d0cc2bfc9f2623d1a80ef30f2bebef16
Семён Семёнов <semyon_semyonov_2003@bk.ru>
2023-02-19 04:00:39 +0400

    review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from .models import Comment, Review
+from .serializers_2 import CommentSerializer, ReviewSerializer
 
-# Create your views here.
+
 class CommentViewSet(viewsets.ModelViewSet):
     """Представление модели комменатриев."""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
 
-    def get_queryset(self):
-        return super().get_queryset().filter(
-            post_id=self.kwargs.get('post_id')
-        )
 
-    def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        serializer.save(author=self.request.user, post=post)
-
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Представление модели отзывов"""
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('name',)
+    # permission_classes = (IsAdminOrReadOnly,)
